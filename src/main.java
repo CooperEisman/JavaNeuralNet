73979e3a06a923529cdea0a8b4a9dd1c9f967f2e
ceisman21 <ceisman21@kentdenver.org>
2021-02-26 01:16:41 -0700

    Quick Change for sourcing

--- src/main.java
+++ src/main.java
@@ -1,9 +1,11 @@
 public class main {
 
     public static void main(String[] args) {
+        //Values
         int numInputs = 284;
         int numOutputs = 10;
         int numLayers = 25;
+        System.out.println("Neural Net for Image->Numerical Recognition. Code by Cooper Eisman, Dataset from MNIST. Sourcing in Readme");
 
 
         //Create a net
